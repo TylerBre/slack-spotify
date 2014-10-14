a7e9459c3716c02a1604ed92fb9666fc1aa920d6
Riccardo Franconi <riccardo@MacBook-Pro-di-Riccardo-2.local>
2014-10-14 09:59:03 +0200

    adds gulp to automating build

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins(),
+    runSequence = require('run-sequence');
+
+gulp.task('jshint', function () {
+    return gulp.src('modules/*.js')
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('default'));
+});
+
+gulp.task('tests', function () {
+    return gulp.src('tests/*/*.js', {read: false})
+        .pipe(plugins.mocha({reporter: 'spec'}));
+});
+
+gulp.task('npm:install', plugins.shell.task(['npm install']));
+gulp.task('npm:start', plugins.shell.task(['npm start']));
+
+gulp.task('build', function () {
+    runSequence(
+        ['jshint', 'tests'],
+        'npm:install',
+        'npm:start'
+    );
+});
